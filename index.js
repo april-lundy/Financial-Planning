945a3620a72610165715563d4a63408dcf421fdb
April Lundy <april.t.lundy@gmail.com>
2016-07-17 09:12:48 -0400

    Moved the sync function outside of the data layer and into the user layer

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // Financial Planning Software
 
 // Calculate current net worth by tracking assests and debt
+
+var models = require('./models');
+
+models.connection.sync()
+	.then(function() {
+		return models.LiquidAsset.create({
+			name: "Savings",
+			value: 0,
+			interest_rate: 0,
+			compound_period: 0
+
+		})
+
+	})
+	.then(function(record) {
+		console.log(record.toJSON());
+	})
--- models.js
+++ models.js
@@ -9,7 +9,7 @@ var connection = new Sequelize({
 
 });
 
-var LiquidAssest = connection.define("liquid_asset", {
+var LiquidAsset = connection.define("liquid_asset", {
 
 	name: Sequelize.STRING,
 	value: Sequelize.INTEGER,
@@ -17,7 +17,7 @@ var LiquidAssest = connection.define("liquid_asset", {
 	compound_period: Sequelize.INTEGER
 });
 
-var StaticAssest = connection.define("static_asset", {
+var StaticAsset = connection.define("static_asset", {
 
 	name: Sequelize.STRING,
 	value: Sequelize.INTEGER,
@@ -25,24 +25,16 @@ var StaticAssest = connection.define("static_asset", {
 	compound_period: Sequelize.INTEGER
 });
 
-var RecurringAssest = connection.define("recurring_asset", {
+var RecurringAsset = connection.define("recurring_asset", {
 
 	name: Sequelize.STRING,
 	value: Sequelize.INTEGER,
 	recurrence_period: Sequelize.INTEGER
 });
 
-connection.sync()
-	.then(function() {
-		return LiquidAssest.create({
-			name: "Savings",
-			value: 0,
-			interest_rate: 0,
-			compound_period: 0
-
-		})
-
-	})
-	.then(function(record) {
-		console.log(record.toJSON());
-	})
+module.exports = {
+	connection: connection,
+	LiquidAsset: LiquidAsset,
+	StaticAsset: StaticAsset,
+	RecurringAsset: RecurringAsset,
+}
